3a5527e5b25546f0d6e5b5f8e5ba2d7b9e92bd6f
FernandoCostaB <fbc@icomp.ufam.edu.br>
2019-02-17 19:07:11 -0400

    ordenando posts

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -12,62 +12,64 @@ export class DataBase {
 
     public publicar(publicacao: any): void {
         firebase.database().ref(`posts/${btoa(publicacao.email)}`)
-            .push({titulo: publicacao.titulo })
+            .push({ titulo: publicacao.titulo })
             .then((resposta: any) => {
 
                 const nomeImagem = resposta.key;
 
                 firebase.storage().ref()
-                .child(`imagens/${nomeImagem}`)
-                .put(publicacao.imagem)
-                .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                    (snapshot: any) => {
-                        this.progresso.status = 'andamento';
-                        this.progresso.estado = snapshot;
-                        //  console.log('captura feita no serviço da base de dados: ', snapshot);
-                    },
-                    (_error: any) => {
-                        this.progresso.status = 'erro';
-                    },
-                    () => {
-                        this.progresso.status = 'concluido';
-                    }
+                    .child(`imagens/${nomeImagem}`)
+                    .put(publicacao.imagem)
+                    .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                        (snapshot: any) => {
+                            this.progresso.status = 'andamento';
+                            this.progresso.estado = snapshot;
+                            //  console.log('captura feita no serviço da base de dados: ', snapshot);
+                        },
+                        (_error: any) => {
+                            this.progresso.status = 'erro';
+                        },
+                        () => {
+                            this.progresso.status = 'concluido';
+                        }
                     );
             });
     }
 
-    public consultarPublicacoes(email: string): Promise <any> {
+    public consultarPublicacoes(email: string): Promise<any> {
 
-        return new Promise ((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             firebase.database().ref(`posts/${btoa(email)}`)
-            .once('value')
-            .then((snapshot: any) => {
-               // lista de publicações
-               const publicacoes: Array<any> = [];
+                .once('value')
+                .then((snapshot: any) => {
+                    // lista de publicações
+                    const publicacoes: Array<any> = [];
 
-                snapshot.forEach((childSnapshot: any) => {
-                    const publicacao = childSnapshot.val();
-                   //  recuperando a imagem do post
-                   firebase.storage().ref().child(`imagens/${childSnapshot.key}`)
-                   .getDownloadURL()
-                   .then((url: string) => {
-                       publicacao.url_imagem = url;
-                       // recuperando o nome do usuario
-                       firebase.database().ref(`usuario_detalhe/${btoa(email)}`)
-                       .once('value')
-                       .then((snapshot: any) => {
-                           publicacao.nome_usuario = snapshot.val().nome_usuario;
-                           publicacoes.push(publicacao);
-                       });
+                    snapshot.forEach((childSnapshot: any) => {
+                        const publicacao = childSnapshot.val();
+                        publicacao.key = childSnapshot.key;
+                        publicacoes.push(publicacao);
+                    });
+                    //   resolve(publicacoes);
+                    return publicacoes.reverse();
+                })
+                .then((publicacoes: any) => {
+                    publicacoes.forEach((publicacao) => {
+                        firebase.storage().ref().child(`imagens/${publicacao.key}`)
+                            .getDownloadURL()
+                            .then((url: string) => {
+                                publicacao.url_imagem = url;
+                                // recuperando o nome do usuario
+                                firebase.database().ref(`usuario_detalhe/${btoa(email)}`)
+                                    .once('value')
+                                    .then((snapshot: any) => {
+                                        publicacao.nome_usuario = snapshot.val().nome_usuario;
+                                    });
 
-                   });
+                            });
+                    });
+                resolve(publicacoes);
                 });
-              resolve(publicacoes);
-            });
         });
-
-
-
     }
-
 }
